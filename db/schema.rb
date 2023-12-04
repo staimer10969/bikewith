36f20119d12ecc1038187984044fae673ae7f8d7
staimer10969 <@users.noreply.github.com>
2023-12-04 11:52:20 +0000

    [Fix] 本番環境でのエラー修正のためmigrate順番変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_23_075046) do
+ActiveRecord::Schema.define(version: 2023_07_23_076947) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
